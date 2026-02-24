09fef6666a44b3a169bd05c3c2f852a443718b6a
sainath gaidhankar <sainathgaidhankar@gmail.com>
2026-02-24 17:41:42 +0530

    host and upload images

---
+++ app/routes/visualizer.$id.tsx
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router"
+
+const visualizerID = () => {
+    const location = useLocation();
+    const { initialImage, name } = location.state || {};
+  return (
+    <section>
+        <h1>{name || 'Untitled Project' }</h1>
+        <div className="visualizer" >
+            {initialImage && (
+                <div className="image-container" >
+                    <h2>Source Image</h2>
+                    <img src={initialImage} alt="source" />
+                </div>
+            )}
+        </div>
+    </section>
+  )
+}
+
+export default visualizerID
